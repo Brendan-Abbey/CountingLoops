f7843f731a8eafdf999ebb0585a47da6c17724c0
Brendan-Abbey <brendan8abb@gmail.com>
2022-05-03 14:27:09 -0400

    Header Update

--- src/countingloops/CountingLoops.java
+++ src/countingloops/CountingLoops.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Brendan Abbey
+ * May 2 2022
+ * Counting Loops
  */
 package countingloops;
 import java.util.Scanner;
